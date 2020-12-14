e7c975fc0f679534da4fa79e37b3254817feb03e
H43RO <h2is1234@naver.com>
2020-12-14 14:00:00 +0900

    Grammer\n

--- 01.Grammer/01.Number.py
+++ 01.Grammer/01.Number.py
@@ -1,1 +1,56 @@
-print("asdf")
+# 정수형
+a = 1000
+print(a)
+
+a = -7
+print(a)
+
+a = 0
+print(a)
+
+a = a + 5
+print(a)
+
+a = a + 3
+print(a)
+
+a = a - 2
+print(a)
+
+
+# 실수형
+a = 157.93
+print(a)
+
+a = -182.24
+print(a)
+
+a = 5.
+print(a)
+
+a = -.7
+print(a)
+
+a = 1e9
+print(a)
+
+a = 75.25e1
+print(a)
+
+a = int(1e9)
+print(a)
+
+a = 0.3 + 0.6  # 미세한 오류로 0.9 가 나오지 않음
+print(a)
+
+a = round(a, 2)  # round() 를 통해 셋째 자리에서 반올림하여 절충
+print(a)
+
+a = 5 / 2
+print(a)
+
+a = 5 // 2
+print(a)
+
+a = 5 % 2
+print(a)
