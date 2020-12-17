6ef0bc4fabb356385ca7d4228c79a7d911b5ec13
H43RO <h2is1234@naver.com>
2020-12-17 14:00:00 +0900

    String

---
+++ 01.Grammer/03.String.py
@@ -0,0 +1,15 @@
+data = 'Hello World'
+print(data)
+
+data = "Don't you Know \"Python\"?"  # Escape Sequence
+print(data)
+print(data[0:5])
+# data[3] = 'a' -> 불가능 (Immutable)
+
+a = "Hello"
+b = "World"
+print(a + " " + b)
+
+a = "String "
+print(a * 3)
+
