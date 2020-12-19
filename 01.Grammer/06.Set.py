cdad3a33c6c7f0d1fbaffda5fd5d168fbc7bbcab
H43RO <h2is1234@naver.com>
2020-12-19 14:00:00 +0900

    Set

--- 01.Grammer/06.Set.py
+++ 01.Grammer/06.Set.py
@@ -3,3 +3,24 @@ print(data)
 
 data = {1, 1, 2, 3, 4, 4, 5}
 print(data)
+print()
+
+a = {1,2,3}
+print(a)
+
+a.add(4)
+print(a)
+
+a.update([5,6])
+print(a)
+
+a.remove(3)
+print(a)
+print()
+
+a = {1,2,3,4,5}
+b = {3,4,5,6,7}
+
+print(a | b)  # 합집합
+print(a & b)  # 교집합
+print(a - b)  # 차집합
