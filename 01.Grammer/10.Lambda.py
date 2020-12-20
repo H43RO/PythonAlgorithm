ed8a922249df75704b1bfe3124eddc1f8220b5a5
H43RO <h2is1234@naver.com>
2020-12-20 21:04:04 +0900

    Lambda

--- 01.Grammer/10.Lambda.py
+++ 01.Grammer/10.Lambda.py
@@ -50,3 +50,24 @@ def operator(a, b):
 
 a, b, c, d = operator(8, 4)
 print(a, b, c, d)
+
+# Lambda
+
+print((lambda a, b: a + b)(3, 7))  # 함수를 입력으로 받는 함수에 유용하게 사용됨
+
+array = [('홍길동', 50), ('이순신', 32), ('아무개', 74)]
+
+
+def my_key(x):
+    return x[1]
+
+
+print(sorted(array, key=my_key))
+print(sorted(array, key=lambda x: x[1]))
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [6, 7, 8, 9, 10]
+
+result = map(lambda a, b: a + b, list1, list2)
+print(list(result))
+
