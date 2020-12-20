069e292f5d16023252e846d6e5f323ca7d2c3ceb
H43RO <h2is1234@naver.com>
2020-12-20 20:29:21 +0900

    Condition

--- 01.Grammer/08.Condition.py
+++ 01.Grammer/08.Condition.py
@@ -8,8 +8,30 @@ if x >= 0:
 
 if x >= 30:
     print("x >= 30")
+elif x >= 20:
+    print("x >= 20")
 else:
-    print("x <= 30")
+    print("x <= 20, x <= 30")
 
-print("End")
+if True or False:
+    print("YES")
 
+if True and False:
+    print("YES")
+
+a = 15
+
+if a <= 20 and a >= 0:
+    print("YES")
+
+if 0 <= a <= 20:
+    print("YES!")
+
+score = 85
+if score >= 80:
+    pass  # NOP (디버깅에 사용)
+else:
+    print('성적이 80점 미만입니다')
+
+result = "Success" if score >= 80 else "Fail"
+print(result)
