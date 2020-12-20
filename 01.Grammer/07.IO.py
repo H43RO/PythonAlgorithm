a78f4b41762f663a7f209a11c63d1d6241a84d4a
H43RO <h2is1234@naver.com>
2020-12-20 20:15:33 +0900

    Condition

--- 01.Grammer/07.IO.py
+++ 01.Grammer/07.IO.py
@@ -23,5 +23,5 @@ print(8, end=" ")
 
 answer = 7
 print("정답은 " + str(answer) + "입니다.")
-
+print(f"정답은 {answer}입니다.")
 
---
+++ 01.Grammer/08.Condition.py
@@ -0,0 +1,15 @@
+x = 15
+
+if x >= 10:
+    print("x >= 10")
+
+if x >= 0:
+    print("x >= 0")
+
+if x >= 30:
+    print("x >= 30")
+else:
+    print("x <= 30")
+
+print("End")
+
