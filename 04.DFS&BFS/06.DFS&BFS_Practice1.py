058a93974521ccc5057b295ea39cfabb9533b481
H43RO <h2is1234@naver.com>
2020-12-26 21:19:34 +0900

    DFS & BFS Practice

--- 04.DFS&BFS/06.DFS&BFS_Practice1.py
+++ 04.DFS&BFS/06.DFS&BFS_Practice1.py
@@ -23,19 +23,24 @@
 
 n, m = map(int, input().split())
 
+# 2차원 리스트의 맵 정보 입력
 graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
 
 result = 0
 
-
+# DFS 로 특정 노드를 방문하고 연결된 모든 노드들도 방문
 def dfs(x, y):
+    # 주어진 범위를 벗어나는 경우 즉시 종료
     if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
 
+    # 현재 노드를 아직 방문하지 않았다면
     if graph[x][y] == 0:
+        # 해당 노드 방문 처리
         graph[x][y] = 1
+        # 상하좌우 위치들도 모두 재귀적으로 호출
         dfs(x - 1, y)
         dfs(x, y - 1)
         dfs(x + 1, y)
@@ -43,6 +48,7 @@ def dfs(x, y):
         return True
     return False
 
+# 모든 노드에 대하여 음료수 채우기
 for i in range(n):
     for j in range(m):
         if dfs(i, j) == True:
