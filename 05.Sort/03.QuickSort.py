1b8372d096942fe96b03dd513758fdac54f9c0d2
H43RO <h2is1234@naver.com>
2020-12-29 00:24:36 +0900

    Quick Sort Algorithm

---
+++ 05.Sort/03.QuickSort.py
@@ -0,0 +1,40 @@
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    while left <= right:
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+        while right > start and array[right] >= array[pivot]:
+            right -= 1
+        if left > right:
+            array[right], array[pivot] = array[pivot], array[right]
+        else:
+            array[left], array[right] = array[right], array[left]
+    quick_sort(array, start, right - 1)
+    quick_sort(array, right + 1, end)
+
+
+quick_sort(array, 0, len(array) - 1)
+print(array)
+
+
+# 파이썬의 List Comprehension 과 Slicing 활용하여 간편하게 구현한 퀵 소트
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+    pivot = array[0]
+    tail = array[1:]
+
+    left_side = [x for x in tail if x <= pivot]
+    right_side = [x for x in tail if x > pivot]
+
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+
+print(quick_sort(array))
