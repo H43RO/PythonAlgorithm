a6bdd9d9c77ecd48470cf091121de7a56a42cac1
H43RO <h2is1234@naver.com>
2021-01-12 23:17:54 +0900

    [BOJ] 1947

---
+++ 00.Solve/BOJ/1000-2000/1947.py
@@ -0,0 +1,24 @@
+def solve(n):
+    if n == 1:
+        return False
+
+    if n == 2:
+        return True
+
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+result = 0
+
+for i in arr:
+    if solve(i):
+        result += 1
+
+print(result)
