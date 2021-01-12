4aa43b6ce375065959286537fc44b503ece28983
H43RO <h2is1234@naver.com>
2021-01-12 18:27:32 +0900

    [BOJ] 1149 설명추가

--- 00.Solve/BOJ/1000-2000/1149.py
+++ 00.Solve/BOJ/1000-2000/1149.py
@@ -4,15 +4,17 @@
 # 부분 문제의 해결 성질에 따라 맨 마지막 집의 색칠 비용 중 최솟값이 결국 정답이 된다.
 
 def paint(n, houses):
+    # 첫 번째 집을 제외한 모든 집에 대하여 모든 색칠 비용을 고려
     for i in range(2, n + 1):
         for x in range(3):
-            if x == 0:
+            if x == 0:  # R 을 탐색할 차례에는 이전 집의 G, B 중 최솟값을 더해줌
                 houses[i][x] += min(houses[i - 1][1], houses[i - 1][2])
-            if x == 1:
+            if x == 1:  # G 를 탐색할 차례에는 이전 집의 R, B 중 최솟값을 더해줌
                 houses[i][x] += min(houses[i - 1][0], houses[i - 1][2])
-            if x == 2:
+            if x == 2:  # B 를 탐색할 차례에는 이전 집의 R, G 중 최솟값을 더해줌
                 houses[i][x] += min(houses[i - 1][0], houses[i - 1][1])
 
+    # 부분 문제의 성질에 따라 마지막 집의 최소 색칠 비용이 곧 정답
     return min(houses[n])
 
 
