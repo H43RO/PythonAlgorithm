f688a9f7179afe697a663f710d4d619a63a57ad9
H43RO <h2is1234@naver.com>
2021-01-23 00:01:29 +0900

    [BOJ] 1931

---
+++ 00.Solve/BOJ/1000-2000/1931.py
@@ -0,0 +1,26 @@
+from sys import stdin, stdout
+from collections import deque
+
+n = int(stdin.readline())
+meeting = []
+
+for i in range(n):
+    meeting.append(list(map(int, stdin.readline().split())))
+
+# 끝나는 시간이 빠른 순으로 정렬 (끝나는 시간이 같으면 시작하는 시간이 빠른 순으로 정렬)
+meeting.sort(key=lambda x: (x[1], x[0]))
+
+meeting = deque(meeting)
+result = 1
+end_time = meeting.popleft()[1]
+
+while meeting:
+    # 끝 시간과 시작 시간이 같은 경우도 회의가 가능한건지 모르고
+    # meeting[0][0] > end_time 이라고 했다가 오답 처리 됨
+    if meeting[0][0] >= end_time:  # 회의 시작 시간이 이전 회의 종료 시간 이후라면
+        result += 1
+        end_time = meeting.popleft()[1]
+    else:
+        meeting.popleft()
+
+print(result)
