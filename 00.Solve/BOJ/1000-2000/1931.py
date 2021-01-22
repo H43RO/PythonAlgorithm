4184d9a46d6533d812114726dea517bf13fab877
H43RO <h2is1234@naver.com>
2021-01-23 00:30:52 +0900

    [BOJ] 3046, 2163, 10699, 2914

--- 00.Solve/BOJ/1000-2000/1931.py
+++ 00.Solve/BOJ/1000-2000/1931.py
@@ -8,6 +8,8 @@ for i in range(n):
     meeting.append(list(map(int, stdin.readline().split())))
 
 # 끝나는 시간이 빠른 순으로 정렬 (끝나는 시간이 같으면 시작하는 시간이 빠른 순으로 정렬)
+# 맨 첫 회의를 고르는 상황에 왜냐하면 끝나는 시간이 같은데 (3, 3) (1, 3) 와 같이 입력됐다면
+# 정렬 결과 상 (1, 3) 을 고르지 않고 (3, 3) 을 골라버리기 때문에 손해가 발생하기 때문
 meeting.sort(key=lambda x: (x[1], x[0]))
 
 meeting = deque(meeting)
