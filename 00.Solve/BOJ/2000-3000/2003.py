09d2157663b9144af876782e90221fe50f603b1a
H43RO <h2is1234@naver.com>
2021-01-24 22:31:33 +0900

    [BOJ] 1806

--- 00.Solve/BOJ/2000-3000/2003.py
+++ 00.Solve/BOJ/2000-3000/2003.py
@@ -3,7 +3,7 @@ from sys import stdin, stdout
 n, s = map(int, stdin.readline().split())
 num = list(map(int, stdin.readline().split()))
 
-result = []
+result = 0
 end = 0
 temp_sum = 0
 
@@ -11,11 +11,8 @@ for start in range(n):
     while temp_sum < s and end < n:
         temp_sum += num[end]
         end += 1
-    if temp_sum >= s:
-        result.append(end - start)
+    if temp_sum == s:
+        result += 1
     temp_sum -= num[start]
 
-if result:
-    print(min(result))
-else:
-    print(0)
+print(result)
