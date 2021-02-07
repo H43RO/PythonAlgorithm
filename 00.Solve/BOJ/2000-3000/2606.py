c8fe4814f87325bf4cae354c39861bc1d9611242
H43RO <h2is1234@naver.com>
2021-02-07 14:00:00 +0900

    [BOJ] 2606, 2667 해설

--- 00.Solve/BOJ/2000-3000/2606.py
+++ 00.Solve/BOJ/2000-3000/2606.py
@@ -1,29 +1,39 @@
 n = int(input())
 edge = int(input())
 
+# 노드 간 연결 정보를 담을 그래프 (2차원 리스트) 선언
 graph = [[] * (n + 1) for _ in range(n + 1)]
+# 방문 정보를 담을 리스트 선언
 visited = [False] * (n + 1)
 result = 0
 
+# 노드 정보를 담을 그래프 만들기
 for i in range(edge):
     first, second = map(int, input().split())
     graph[first].append(second)
     graph[second].append(first)
 
+# 각 노드의 인접 노드 정렬
 for x in graph:
     x.sort()
 
 
 def dfs_function(graph, v):
     global result
+    # 우선 입력받은 시작 노드는 방문 처리
     visited[v] = True
+    # 감염 횟수 증가
     result += 1
+    # 입력받은 시작 노드의 모든 인접 노드들에 대하여
     for i in graph[v]:
+        # 만약 해당 인접 노드를 방문한 적 없다면
         if not visited[i]:
+            # 해당 노드를 시작으로 DFS 탐색 시작
             dfs_function(graph, i)
 
 
+# 1번 노드부터 DFS 탐색 시작
 dfs_function(graph, 1)
 
-# 자기 자신 제외
+# 1번 노드 방문 제외한 결과 출력
 print(result-1)
--- 00.Solve/BOJ/2000-3000/2667.py
+++ 00.Solve/BOJ/2000-3000/2667.py
@@ -1,10 +1,14 @@
 n = int(input())
 
+# 지도 정보
 graph = []
 
+# 총 단지 수
 result = 0
 
+# 각 단지 내 집의 수를 저장하는 리스트
 counts = []
+# 각 단지 내 집의 수
 count = 0
 
 for i in range(n):
@@ -13,11 +17,16 @@ for i in range(n):
 
 def dfs_function(x, y):
     global count
+    # 지도 밖으로 나가는 경우 False 리턴
     if x <= -1 or x >= n or y <= -1 or y >= n:
         return False
+    # 해당 좌표가 집일 경우
     if graph[x][y] == 1:
+        # count 증가
         count += 1
+        # 이미 탐색했으므로 값 0 으로 변경
         graph[x][y] = 0
+        # 상하좌우 방향에 대하여 DFS 탐색 시작
         dfs_function(x - 1, y)
         dfs_function(x + 1, y)
         dfs_function(x, y - 1)
@@ -25,11 +34,14 @@ def dfs_function(x, y):
         return True
     return False
 
-
+# 모든 좌표에 대하여 DFS 탐색 시도
 for i in range(n):
     for j in range(n):
+        # DFS 결과가 True 라면 (해당 단지가 모두 탐색된 상태)
         if dfs_function(i, j) == True:
+            # 단지 수 1 증가
             result += 1
+            # 단지 내 집의 수 증가하고 저장한 뒤 초기화
             counts.append(count)
             count = 0
 
