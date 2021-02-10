991abd6a7a5f15c9d06b024f00a2f132025888cd
H43RO <h2is1234@naver.com>
2021-02-10 14:00:00 +0900

    [BOJ] 1946

--- 00.Solve/BOJ/1000-2000/1946.py
+++ 00.Solve/BOJ/1000-2000/1946.py
@@ -10,20 +10,18 @@ for _ in range(case):
     for i in range(n):
         people.append(tuple(map(int, stdin.readline().split())))
     people = deque(sorted(people))
-
-    selected = [people.popleft()]
-
-    interview_min = selected[0][1]
-
+    interview_min = people[0][1]
+    people.popleft()
+    count = 1
     # 순위는 낮을 수록 좋은 것
     # 매번 가장 우수한 사람과 교차 비교하여 둘 중 하나라도 우수하면 (값이 더 작으면) 합격
     for x in people:
         if interview_min > x[1]:
-            selected.append(x)
+            count += 1
             interview_min = x[1]
         else:
             continue
-    results.append(len(selected))
+    results.append(count)
 
 for x in results:
     print(x)
