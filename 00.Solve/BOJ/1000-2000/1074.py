43ec0bc4e0a9065de56d88c503df5faca5881fe7
H43RO <h2is1234@naver.com>
2021-02-17 17:32:50 +0900

    [BOJ] 1074

--- 00.Solve/BOJ/1000-2000/1074.py
+++ 00.Solve/BOJ/1000-2000/1074.py
@@ -1,40 +1,35 @@
-n, r, c = map(int, input().split())
-result = 0
+from sys import stdin, setrecursionlimit
+
+setrecursionlimit(10000)
 
 
-def solve(n, x, y):
-    global result
-
-    if n == 2:
+def solve(n, x, y, cnt):
+    if n == 1:
         if x == r and y == c:
-            print(result)
-            return
-
-        result += 1
-
-        if x == r and y + 1 == c:
-            print(result)
-            return
-
-        result += 1
-
-        if x + 1 == r and y == c:
-            print(result)
-            return
-
-        result += 1
-
-        if x + 1 == r and y + 1 == c:
-            print(result)
-
-        result += 1
-
-        return
-
-    solve(n / 2, x, y)
-    solve(n / 2, x, y + n / 2)
-    solve(n / 2, x + n / 2, y)
-    solve(n / 2, x + n / 2, y + n / 2)
+            return cnt
+        elif x == r and y + 1 == c:
+            return cnt + 1
+        elif x + 1 == r and y == c:
+            return cnt + 2
+        else:
+            return cnt + 3
+    else:
+        if x + 2 ** (n - 1) > r:
+            if y + 2 ** (n - 1) > c:
+                # 1 사분면으로 이동
+                return solve(n - 1, x, y, cnt)
+            else:
+                # 2 사분면으로 이동
+                return solve(n - 1, x, y + 2 ** (n - 1), cnt + 2 ** (2 * n - 2))
+        else:
+            if y + 2 ** (n - 1) > c:
+                # 3 사분면으로 이동
+                return solve(n - 1, x + 2 ** (n - 1), y, cnt + 2 * 2 ** (2 * n - 2))
+            else:
+                # 4 사분면으로 이동
+                return solve(n - 1, x + 2 ** (n - 1), y + 2 ** (n - 1), cnt + 3 * 2 ** (2 * n - 2))
 
 
-solve(2 ** n, 0, 0)
+n, r, c = map(int, stdin.readline().split())
+result = solve(n, 0, 0, 0)
+print(result)
