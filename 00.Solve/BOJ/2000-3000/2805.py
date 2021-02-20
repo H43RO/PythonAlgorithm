b6457563103647360c799c357a0a6733073bc628
H43RO <h2is1234@naver.com>
2021-02-20 22:44:47 +0900

    [BOJ] 2805

---
+++ 00.Solve/BOJ/2000-3000/2805.py
@@ -0,0 +1,32 @@
+n, m = map(int, input().split())
+# 나무 길이 정보 입력
+data = list(map(int, input().split()))
+
+# 이분 탐색 start, end 정의
+start = 0
+end = max(data)
+
+result = 0
+
+# 모든 구간에 대하여 탐색
+while start <= end:
+    total = 0
+    mid = (start + end) // 2
+
+    # 현재 절단기 높이 (mid) 에 의해 잘려진 나무 길이 총합 계산
+    for x in data:
+        if x >= mid:
+            total += x - mid
+
+    # 만약 잘려진 나무 길이 총합이 요구조건보다 부족하다면
+    if total < m:
+        # 절단기 높이를 낮추기 위해 end 를 mid - 1 로 변경
+        end = mid - 1
+
+    # 만약 잘려진 나무 길이 총합이 요구조건 이상이라면
+    else:
+        # 결과값 저장하고 더 나은 답안이 있을 수 있으니 start 를 mid + 1 로 지정
+        result = mid
+        start = mid + 1
+
+print(result)
