37bb839ffa4a0ab0287ccdc2aac689766ac57d98
H43RO <h2is1234@naver.com>
2021-02-20 23:57:35 +0900

    [BOJ] 12015

---
+++ 00.Solve/BOJ/12000-13000/12015.py
@@ -0,0 +1,18 @@
+from bisect import bisect_left
+from sys import stdin, stdout
+
+LIS = []
+
+n = int(stdin.readline().strip())
+num = list(map(int, input().split()))
+
+for i, v in enumerate(num):
+    if i == 0:
+        LIS.append(v)
+    else:
+        if LIS[len(LIS) - 1] < v:
+            LIS.append(v)
+        else:
+            LIS[bisect_left(LIS, v)] = v
+
+print(len(LIS))
