c489c5e3f5675f8dbb122d80f77b6616bc5badf0
H43RO <h2is1234@naver.com>
2021-02-27 14:00:00 +0900

    [BOJ] 2212

--- 00.Solve/BOJ/2000-3000/2212.py
+++ 00.Solve/BOJ/2000-3000/2212.py
@@ -2,7 +2,23 @@ from sys import stdin, stdout
 
 n = int(stdin.readline())
 k = int(stdin.readline())
+
+# 같은 좌표에 있는 센서 중복 제거
 sensors = list(map(int, stdin.readline().split()))
 sensors.sort()
 
-# 1 3 6 6 7 9
+data = []
+
+for i in range(1, len(sensors)):
+    data.append(sensors[i] - sensors[i - 1])
+
+data.sort()
+
+for i in range(k - 1):
+    if len(data) == 0:
+        data.append(0)
+        break
+    else:
+        data.pop()
+
+print(sum(data))
