6b616795c7eb5c9b0867a332330ce590c1a5b33a
H43RO <h2is1234@naver.com>
2021-03-08 16:50:28 +0900

    Floyd-Warshall

--- 08.ShortestPath/02.Floyd-Warshall.py
+++ 08.ShortestPath/02.Floyd-Warshall.py
@@ -11,29 +11,38 @@
 
 INF = int(1e9)
 
+# 노드의 개수 및 간선의 개수 입력
 n = int(input())
 m = int(input())
 
+# 2차원 리스트 (그래프) 를 만들고, 무한으로 초기화
 graph = [[INF] * (n + 1) for _ in range(n + 1)]
 
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
 for a in range(1, n + 1):
     for b in range(1, n + 1):
         if a == b:
             graph[a][b] = 0
 
+# 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
 for _ in range(m):
+    # a 에서 b 로 가는 비용은 c 라고 설정
     a, b, c = map(int, input().split())
     graph[a][b] = c
 
+# 점화식에 따라 플로이드 워셜 알고리즘 수행
 for k in range(1, n + 1):
     for a in range(1, n + 1):
         for b in range(1, n + 1):
             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
+# 수행 결과
 for a in range(1, n + 1):
     for b in range(1, n + 1):
+        # 간선이 없는 경우 (도달할 수 없는 경우) 에러 출력
         if graph[a][b] == INF:
             print("INFINITY", end=" ")
+        # 도달할 수 있는 경우 최단 거리 출력
         else:
             print(graph[a][b], end=" ")
     print()
