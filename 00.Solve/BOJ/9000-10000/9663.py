9443ad49a6a7f62435cb9ddb13760e9b0ce0ac12
H43RO <h2is1234@naver.com>
2021-03-09 01:04:04 +0900

    [BOJ] 9663

--- 00.Solve/BOJ/9000-10000/9663.py
+++ 00.Solve/BOJ/9000-10000/9663.py
@@ -1,28 +1,25 @@
-def promising(i, col):
-    k = 1
-    flag = True
-    while k < i and flag:
-        if col[i] == col[k] or abs(col[i] - col[k]) == (i - k):
-            flag = False
-        k += 1
-    return flag
+def is_promising(x):
+    for i in range(1, x):
+        # 행, 열, 대각선 중 경로가 겹치는 퀸이 발견되면 non-promising 한 것
+        if col[x] == col[i] or abs(col[x] - col[i]) == x - i:
+            return False
+    return True
 
 
-def n_queens(i, col):
+def n_queens(count):
     global result
-    n = len(col) - 1
-    if promising(i, col):
-        if i == n:
-            result += 1
-        else:
-            for j in range(1, n + 1):
-                col[i + 1] = j
-                n_queens(i + 1, col)
+    if count > n:
+        result += 1
+    else:
+        for i in range(1, n + 1):
+            col[count] = i
+            if is_promising(count):
+                n_queens(count + 1)
 
 
 n = int(input())
 result = 0
 col = [0] * (n + 1)
-n_queens(0, col)
+n_queens(1)
 
 print(result)
