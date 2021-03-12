fead482bd67244cee2eabd54e5182f47bdb7ff07
H43RO <h2is1234@naver.com>
2021-03-12 20:10:16 +0900

    [Programmers] 전화 번호 목록

---
+++ 00.Solve/Programmers/고득점_Kit/Hash/전화번호_목록.py
@@ -0,0 +1,9 @@
+def solution(phone_book):
+    phone_book = sorted(phone_book)
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
+
+
+print(solution(["119", "97674223", "1195524421"]))
