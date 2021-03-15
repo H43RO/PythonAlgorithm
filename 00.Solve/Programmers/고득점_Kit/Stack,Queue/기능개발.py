bfc4fe1fb91d9aed7e446630acb4bb582971cdfa
H43RO <h2is1234@naver.com>
2021-03-16 00:30:07 +0900

    [Programmers] 기능 개발

--- 00.Solve/Programmers/고득점_Kit/Stack,Queue/기능개발.py
+++ 00.Solve/Programmers/고득점_Kit/Stack,Queue/기능개발.py
@@ -13,7 +13,7 @@ def solution(progresses, speeds):
             progresses[i] += speeds[i]
 
         temp = 0
-        while progresses and progresses[0] > 100:
+        while progresses and progresses[0] >= 100:
             progresses.popleft()
             speeds.popleft()
             temp += 1
@@ -30,3 +30,4 @@ print(solution([99, 99, 99], [1, 1, 1]))
 print(solution([99, 99, 99, 99, 99], [99, 99, 99, 99, 99]))  # 5
 print(solution([20, 99, 93, 30, 55, 10], [5, 10, 1, 1, 30, 5]))  # 3, 3
 print(solution([1, 99], [99, 1]))  # 2
+print(solution([98, 99, 97, 96], [1, 1, 1, 1]))  # 2, 1, 1
