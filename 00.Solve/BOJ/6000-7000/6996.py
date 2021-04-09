eae313d1b70d4a3d165413e3706d74f22b28ffad
H43RO <h2is1234@naver.com>
2021-04-09 15:51:46 +0900

    [BOJ] 6996, 5525

--- 00.Solve/BOJ/6000-7000/6996.py
+++ 00.Solve/BOJ/6000-7000/6996.py
@@ -5,6 +5,10 @@ n = int(stdin.readline())
 for _ in range(n):
     a, b = stdin.readline().strip().split()
 
+    if len(a) != len(b):
+        print(f"{a} & {b} are NOT anagrams.")
+        continue
+
     # 두 문자열을 오름차순으로 정렬해봤을 때 원소가 모두 같다면 애너그램
     if sorted(a) == sorted(b):
         print(f"{a} & {b} are anagrams.")
