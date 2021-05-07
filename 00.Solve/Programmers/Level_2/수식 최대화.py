4cb02742e14803d74bc949c970b4c1b00f6572c1
H43RO <h2is1234@naver.com>
2021-05-07 22:50:31 +0900

    [Programmers] 수식 최대화

---
+++ 00.Solve/Programmers/Level_2/수식 최대화.py
@@ -0,0 +1,21 @@
+from itertools import permutations
+import re
+
+
+def solution(expression):
+    expressions = set(re.findall("\D", expression))
+    prior = permutations(expressions)
+    cand = []
+
+    for op_cand in prior:
+        temp = re.compile("(\D)").split('' + expression)
+        for exp in op_cand:
+            while exp in temp:
+                idx = temp.index(exp)
+                temp = temp[:idx - 1] + [str(eval(''.join(temp[idx - 1:idx + 2])))] + temp[idx + 2:]
+        cand.append(abs(int(temp[0])))
+    return max(cand)
+
+
+print(solution("100-200*300-500+20"))
+print(solution("50*6-3*2"))
