59ed85c5d278f81adf43033322b48b3928bf06f1
H43RO <h2is1234@naver.com>
2021-05-07 23:03:52 +0900

    [Programmers] 수식 최대화

--- 00.Solve/Programmers/Level_2/수식 최대화.py
+++ 00.Solve/Programmers/Level_2/수식 최대화.py
@@ -3,18 +3,18 @@ import re
 
 
 def solution(expression):
-    expressions = set(re.findall("\D", expression))
-    prior = permutations(expressions)
-    cand = []
+    operators = set(re.findall("\D", expression))
+    priority = permutations(operators)
+    result = []
 
-    for op_cand in prior:
+    for operator in priority:
         temp = re.compile("(\D)").split('' + expression)
-        for exp in op_cand:
+        for exp in operator:
             while exp in temp:
-                idx = temp.index(exp)
-                temp = temp[:idx - 1] + [str(eval(''.join(temp[idx - 1:idx + 2])))] + temp[idx + 2:]
-        cand.append(abs(int(temp[0])))
-    return max(cand)
+                i = temp.index(exp)
+                temp = temp[:i - 1] + [str(eval(''.join(temp[i - 1:i + 2])))] + temp[i + 2:]
+        result.append(abs(int(temp[0])))
+    return max(result)
 
 
 print(solution("100-200*300-500+20"))
