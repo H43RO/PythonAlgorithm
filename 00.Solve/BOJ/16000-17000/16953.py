1b67439ab7e27c2a9f86807c01bb9c9aff905e01
H43RO <h2is1234@naver.com>
2021-05-09 23:38:15 +0900

    [BOJ] 16953

--- 00.Solve/BOJ/16000-17000/16953.py
+++ 00.Solve/BOJ/16000-17000/16953.py
@@ -1,12 +1,17 @@
 a, b = map(int, input().split())
 count = 0
 
+# B 를 A 로 바꾸는 방식의 로직
 while b > a:
     count += 1
+
+    # 만약 1로 끝나는 숫자라면 맨 뒷자리 제거
     if str(b).endswith('1'):
         b = int(str(b)[:-1])
+    # 만약 2로 나누어 떨어지는 숫자라면 2로 나눔
     elif b % 2 == 0:
         b = b // 2
+    # 2가지 경우의 수에 모두 해당 안되면 구제불능
     else:
         break
 
---
+++ 00.Solve/Programmers/고득점_Kit/Greedy/구명보트.py
@@ -0,0 +1,15 @@
+# 구명보트를 최대한 적게 사용하여 모든 사람을 구출하기
+# 단, 보트에는 두명만 탈 수 있음!!
+
+def solution(people, limit):
+    answer = 0
+
+    # 역순 정렬
+    people.sort(reverse=True)
+
+
+    return answer
+
+
+print(solution([70, 50,80, 50], 3))
+print(solution([70, 80, 50], 3))
