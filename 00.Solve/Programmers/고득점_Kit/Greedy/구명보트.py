3c300b41a0024b2030d6e7215b153bbaf7f862e8
H43RO <h2is1234@naver.com>
2021-05-09 23:51:19 +0900

    [Programmers] 구명보트

--- 00.Solve/Programmers/고득점_Kit/Greedy/구명보트.py
+++ 00.Solve/Programmers/고득점_Kit/Greedy/구명보트.py
@@ -1,15 +1,23 @@
 # 구명보트를 최대한 적게 사용하여 모든 사람을 구출하기
 # 단, 보트에는 두명만 탈 수 있음!!
 
+from collections import deque
+
+
 def solution(people, limit):
     answer = 0
 
     # 역순 정렬
-    people.sort(reverse=True)
+    people = deque(sorted(people, reverse=True))
 
+    while people:
+        limit_left = limit - people.popleft()
+        if len(people) >= 1 and people[-1] <= limit_left:
+            people.pop()
+        answer += 1
 
     return answer
 
 
-print(solution([70, 50,80, 50], 3))
-print(solution([70, 80, 50], 3))
+print(solution([70, 50, 80, 50], 100))
+print(solution([70, 80, 50], 100))
