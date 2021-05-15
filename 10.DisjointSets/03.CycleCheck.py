5ef984fecac895cc48435b32fcdb1f819f9a7d6e
H43RO <h2is1234@naver.com>
2021-05-15 18:07:09 +0900

    서로소 집합을 활용한 사이클 체크

---
+++ 10.DisjointSets/03.CycleCheck.py
@@ -0,0 +1,43 @@
+"""
+    유니온 파인드를 통한 사이클 검사
+"""
+
+
+# 특정 원소가 속한 집합 찾기
+def find_parent(parent, x):
+    # 루트 노드를 찾을 때까지 재귀 호출
+    if parent[x] != x:
+        return find_parent(parent, parent[x])
+    return parent[x]
+
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+cycle = False
+
+for i in range(e):
+    a, b = map(int, input().split())
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+        break
+    else:
+        union_parent(parent, a, b)
+
+if cycle:
+    print("사이클이 발생했습니다")
+else:
+    print("사이클이 발생하지 않았습니다")
