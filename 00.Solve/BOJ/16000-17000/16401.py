fecfbd012f82c80490b855d6f36d1b7851274f0e
H43RO <h2is1234@naver.com>
2021-06-19 17:26:33 +0900

    [BOJ] 16401

--- 00.Solve/BOJ/16000-17000/16401.py
+++ 00.Solve/BOJ/16000-17000/16401.py
@@ -3,4 +3,21 @@ from sys import stdin
 m, n = map(int, stdin.readline().split())
 snack = list(map(int, stdin.readline().split()))
 
+start = 1
+end = max(snack)
+
 length = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    count = 0
+    for x in snack:
+        count += (x // mid)
+    if count >= m:
+        length = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+
+
+print(length)
