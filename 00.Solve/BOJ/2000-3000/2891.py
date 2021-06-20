c68262274ec6b3d480a8105f9b29bc7990e625ba
H43RO <h2is1234@naver.com>
2021-06-20 14:00:00 +0900

    [BOJ] 2891

--- 00.Solve/BOJ/2000-3000/2891.py
+++ 00.Solve/BOJ/2000-3000/2891.py
@@ -2,5 +2,17 @@ from sys import stdin
 
 N, S, R = map(int, stdin.readline().split())
 damaged = list(map(int, stdin.readline().split()))
-one_more = list(map(int, stdin.readline().split()))
+left = list(map(int, stdin.readline().split()))
 
+for x in left:
+    if x in damaged:
+        del damaged[damaged.index(x)]
+        continue
+    if x - 1 in damaged:
+        del damaged[damaged.index(x - 1)]
+        continue
+    if x + 1 in damaged:
+        del damaged[damaged.index(x + 1)]
+        continue
+
+print(len(damaged))
