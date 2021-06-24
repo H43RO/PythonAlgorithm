67dbbf17626c883bf1ec2c82ee8b82b7a04b83f5
H43RO <h2is1234@naver.com>
2021-06-24 16:01:49 +0900

    [BOJ] 10830

---
+++ 00.Solve/BOJ/10000-11000/10830.py
@@ -0,0 +1,29 @@
+from sys import stdin
+
+
+def power(m, y):
+    if y == 1:
+        return m
+    partial = power(m, y // 2)
+    partial_square = multiply(partial, partial)
+    if y % 2 == 0:
+        return partial_square
+    elif y % 2 == 1:
+        return multiply(partial_square, m)
+
+
+def multiply(matrix_1, matrix_2):
+    length = len(matrix_1)
+    result = [[0 for _ in range(length)] for _ in range(length)]
+    for i in range(length):
+        for j in range(length):
+            result[i][j] = sum([(matrix_1[i][k] * matrix_2[k][j]) % 1000 for k in range(length)]) % 1000
+    return result
+
+
+a, b = map(int, stdin.readline().split())
+matrix = [list(map(lambda x: x % 1000, map(int, stdin.readline().split()))) for _ in range(a)]
+
+answer = power(matrix, b)
+for i in range(a):
+    print(' '.join(map(str, answer[i])))
---
+++ 00.Solve/BOJ/11000-12000/11444.py
@@ -0,0 +1,5 @@
+from sys import stdin
+
+MOD = 1_000_000_007
+n = int(stdin.readline())
+mat = [[1, 1], [1, 0]]
