bea5320c1daa5c702847de60b308e10afe204bc4
H43RO <h2is1234@naver.com>
2021-06-26 17:28:52 +0900

    [BOJ] 15666

--- 00.Solve/BOJ/15000-16000/15666.py
+++ 00.Solve/BOJ/15000-16000/15666.py
@@ -7,13 +7,6 @@ num.sort()
 data = [0] * (m + 1)
 
 
-def is_promising(data, i):
-    for j in range(1, i + 1):
-        if data[j] >= data[i] and i != j:
-            return False
-    return True
-
-
 def solve(data, count):
     if count > m:
         for i in range(1, m + 1):
@@ -22,8 +15,7 @@ def solve(data, count):
     else:
         for x in num:
             data[count] = x
-            if is_promising(data, count):
-                solve(data, count + 1)
+            solve(data, count + 1)
 
 
 solve(data, 1)
