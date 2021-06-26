a04a4121a03c2a250610b85dfa1928f704f51e8a
H43RO <h2is1234@naver.com>
2021-06-26 17:51:50 +0900

    [BOJ] 15664

--- 00.Solve/BOJ/15000-16000/15664.py
+++ 00.Solve/BOJ/15000-16000/15664.py
@@ -1,14 +1,11 @@
 from itertools import combinations
 
 N, M = map(int, input().split())
-data = list(map(int, input().split()))
-data = sorted(data)
+data = sorted(list(map(int, input().split())))
 result = []
 
 for x in list(combinations(data, M)):
-    if not result:
-        result.append(x)
-    elif x not in result:
+    if x not in result:
         result.append(x)
 
 for x in result:
