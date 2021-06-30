7295676d9eca7dfe73f9714ff7d197ff069a4b95
H43RO <h2is1234@naver.com>
2021-06-30 15:53:43 +0900

    [BOJ] 1743

---
+++ 00.Solve/BOJ/1000-2000/1743.py
@@ -0,0 +1,40 @@
+from sys import stdin
+from collections import deque
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+
+def bfs(graph, x, y):
+    queue = deque([(x, y)])
+    graph[x][y] = 0
+    result = 1
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] != 0:
+                queue.append((nx, ny))
+                graph[nx][ny] = 0
+                result += 1
+
+    return result
+
+
+n, m, k = map(int, stdin.readline().split())
+graph = [[0] * m for _ in range(n)]
+waste = []
+result = 0
+
+for _ in range(k):
+    x, y = map(int, stdin.readline().split())
+    graph[x - 1][y - 1] = 1
+    waste.append((x - 1, y - 1))
+
+for x in waste:
+    i, j = x
+    if graph[i][j] == 1:
+        result = max(result, bfs(graph, i, j))
+
+print(result)
