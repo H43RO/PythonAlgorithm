952672344c5bf87673007eb0ffb82538f4dbe9d3
김현준 <mac-pro@kakao.com>
2021-07-05 20:50:16 +0900

    [BOJ] 11058

--- 00.Solve/BOJ/11000-12000/11058.py
+++ 00.Solve/BOJ/11000-12000/11058.py
@@ -1,7 +1,7 @@
 from sys import stdin
 
 n = int(stdin.readline())
-dp = [i for i in range(101)]
-for i in range(6, 101):
+dp = [i for i in range(n + 1)]
+for i in range(6, n + 1):
     dp[i] = max(dp[i - 5] * 4, dp[i - 4] * 3, dp[i - 3] * 2)
 print(dp[n])
