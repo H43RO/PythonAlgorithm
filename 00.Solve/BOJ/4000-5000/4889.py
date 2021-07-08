8451fa07cef6681814f395489691629a361f4ad3
김현준 <mac-pro@kakao.com>
2021-07-08 14:48:53 +0900

    [BOJ] 4889

--- 00.Solve/BOJ/4000-5000/4889.py
+++ 00.Solve/BOJ/4000-5000/4889.py
@@ -1,23 +1,26 @@
 from sys import stdin
 
-count = 1
+count = 1  # 1-based 인덱싱
 while True:
-    result = 0
     data = stdin.readline().strip()
-    if data.startswith('-'):
+    if data.startswith('-'):  # 종료 조건
         break
+
+    result = 0
     stack = []
     for x in data:
+        # 쌍이 있는 괄호가 발견되면 없애줌
         if len(stack) > 0 and stack[-1] == '{' and x == '}':
             stack.pop()
             continue
         stack.append(x)
-    while stack:
-        a = stack.pop()
-        b = stack.pop()
-        if a == b:
+
+    while stack:  # 문자열이 항상 짝수라는 보장이 있기 때문에
+        a = stack.pop()  # 두 개씩 팝해주면서
+        b = stack.pop()  # 그 값을 비교
+        if a == b:  # '{', '{' 혹은 '}', '}' 인 경우 1회 교체 필요
             result += 1
-        else:
+        else:  # '}', '{' 인 경우 2회 교체 필요
             result += 2
     print(f'{count}. {result}')
 
