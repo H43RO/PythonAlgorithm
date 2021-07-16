541c49b8be8acc3782c32164da7d07c1467c86d5
haero_kim <mac-pro@kakao.com>
2021-07-16 17:04:35 +0900

    [BOJ] 17086

---
+++ 00.Solve/BOJ/17000-18000/17086.py
@@ -0,0 +1,32 @@
+from sys import stdin
+from collections import deque
+
+dx = [-1, -1, -1, 0, 1, 0, 1, 1]
+dy = [-1, 0, 1, 1, 1, -1, 0, -1]
+
+
+def bfs(start_x, start_y):
+    visited = [[-1] * m for _ in range(n)]
+    queue = deque([(start_x, start_y)])
+    visited[start_x][start_y] = 0
+    while queue:
+        x, y = queue.popleft()
+        if graph[x][y] == 1:
+            return visited[x][y]
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == -1:
+                visited[nx][ny] = visited[x][y] + 1
+                queue.append((nx, ny))
+
+
+n, m = map(int, stdin.readline().split())
+graph = [list(map(int, stdin.readline().split())) for _ in range(n)]
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 0:
+            result = max(result, bfs(i, j))
+print(result)
