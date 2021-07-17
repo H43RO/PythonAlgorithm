79181feaf19114a8c79c4266ef7ccad90da1100c
haero_kim <mac-pro@kakao.com>
2021-07-17 19:41:44 +0900

    [BOJ] 2146

---
+++ 00.Solve/BOJ/2000-3000/2146.py
@@ -0,0 +1,46 @@
+from collections import deque
+from sys import stdin, maxsize
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(start):
+    queue = deque()
+    queue.append((start[0], start[1]))
+
+    result = []
+    result.append((start[0], start[1]))
+
+    graph[start[0]][start[1]] = 0
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n:
+                if graph[nx][ny] == 1:
+                    graph[nx][ny] = 0
+                    queue.append((nx, ny))
+                    result.append((nx, ny))
+    return result
+
+
+n = int(stdin.readline())
+graph = [list(map(int, stdin.readline().split())) for _ in range(n)]
+island = []
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] != 0:
+            temp = bfs((i, j))
+            island.append(temp)
+
+result = maxsize
+for i in range(len(island)):
+    for j in range(i + 1, len(island)):
+        for x in range(len(island[i])):
+            for y in range(len(island[j])):
+                result = min(result, abs(island[i][x][0] - island[j][y][0]) + abs(island[i][x][1] - island[j][y][1]))
+
+print(result - 1)
