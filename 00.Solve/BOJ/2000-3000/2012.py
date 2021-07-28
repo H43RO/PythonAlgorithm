565d0d1125cfc5b5c259dbc4fafb917c834478b8
H43RO <h2is1234@naver.com>
2021-07-28 15:14:37 +0900

    [BOJ] 2012, 2251 해설

--- 00.Solve/BOJ/2000-3000/2012.py
+++ 00.Solve/BOJ/2000-3000/2012.py
@@ -2,4 +2,4 @@ from sys import stdin
 
 n = int(stdin.readline())
 data = sorted([int(stdin.readline()) for _ in range(n)])
-print(sum([abs(i - data[i - 1]) for i in range(1, n + 1)]))
+print(sum([abs(i - data[i - 1]) for i in range(1, n + 1)]))  # 정렬된 요소와 그 인덱스 간 차이가 총 불만도
--- 00.Solve/BOJ/2000-3000/2251.py
+++ 00.Solve/BOJ/2000-3000/2251.py
@@ -1,54 +1,58 @@
-
 from collections import deque
 from sys import stdin
 
 MAX_LITER = 201
 a, b, c = map(int, stdin.readline().split())
 
+# A, B 번 용량 상태 각각에 대한 방문 여부 저장하는 2차원 리스트
 visited = [[False] * MAX_LITER for _ in range(MAX_LITER)]
-result = [0 for _ in range(MAX_LITER)]
+result = []  # C 번 물통 용량 모든 경우의 수 저장
 
 queue = deque([(0, 0, c)])
 while queue:
     x, y, z = queue.popleft()
-    if visited[x][y]:
+    if visited[x][y]:  # 해당 A, B 용량 상태를 방문한 적 있으면 패스
         continue
-    visited[x][y] = True
 
-    if x == 0:
-        result[z] = 1
+    visited[x][y] = True  # 방문 처리
 
-    if x + y > b:
-        queue.append((x + y - b, b, z))
-    else:
-        queue.append((0, x + y, z))
+    if x == 0:  # 만약 A 물통이 비어있다면, C 물통에 담긴 물의 양 저장
+        result.append(z)
 
-    if x + z > c:
-        queue.append((x + z - c, y, c))
-    else:
-        queue.append((0, y, x + z))
+    # A 에서 B 로 물 붓는 시나리오 전개
+    if x + y > b:  # A 물통 + B 물통 > B 물통 최대 용량
+        queue.append((x + y - b, b, z))  # B 물통 가득 채우고 남은 만큼 A 남김
+    else:  # A 물통 + B 물통 <= B 물통 최대 용량
+        queue.append((0, x + y, z))  # A 물통에 있는 물 B 물통에 다 때려넣음
 
-    if y + x > a:
-        queue.append((a, y + x - a, z))
-    else:
-        queue.append((y + x, 0, z))
+    # A 에서 C 로 물 붓는 시나리오 전개
+    if x + z > c:  # A 물통 + C 물통 > C 물통 최대 용량
+        queue.append((x + z - c, y, c))  # C 물통 가득 채우고 남은 만큼 A 남김
+    else:  # A 물통 + C 물통 <= C 물통 최대 용량
+        queue.append((0, y, x + z))  # A 물통에 있는 물 C 물통에 다 때려넣음
 
-    if y + z > c:
-        queue.append((x, y + z - c, c))
-    else:
-        queue.append((x, 0, y + z))
+    # B 에서 A 로 물 붓는 시나리오 전개
+    if y + x > a:  # B 물통 + A 물통 > A 물통 최대 용량
+        queue.append((a, y + x - a, z))  # A 물통 가득 채우고 남은 만큼 B 남김
+    else:  # B 물통 + A 물통 <= A 물통 최대 용량
+        queue.append((y + x, 0, z))  # B 물통에 있는 물 A 물통에 다 때려넣음
 
-    if z + x > a:
-        queue.append((a, y, z + x - a))
-    else:
-        queue.append((z + x, y, 0))
+    # B 에서 C 로 물 붓는 시나리오 전개
+    if y + z > c:  # B 물통 + C 물통 > C 물통 최대 용량
+        queue.append((x, y + z - c, c))  # C 물통 가득 채우고 남은 만큼 B 남김
+    else:  # B 물통 + C 물통 <= C 물통 최대 용량
+        queue.append((x, 0, y + z))  # B 물통에 있는 물 C 물통에 다 때려넣음
 
-    if z + y > b:
-        queue.append((x, b, z + y - b))
-    else:
-        queue.append((x, z + y, 0))
+    # C 에서 A 로 물 붓는 시나리오 전개
+    if z + x > a:  # C 물통 + A 물통 > A 물통 최대 용량
+        queue.append((a, y, z + x - a))  # A 물통 가득 채우고 남은 만큼 C 남김
+    else:  # C 물통 + A 물통 <= A 물통 최대 용량
+        queue.append((z + x, y, 0))  # C 물통에 있는 물 A 물통에 다 때려넣음
 
+    # C 에서 B 로 물 붓는 시나리오 전개
+    if z + y > b:  # C 물통 + B 물통 > B 물통 최대 용량
+        queue.append((x, b, z + y - b))  # C 물통 가득 채우고 남은 만큼 B 남김
+    else:  # C 물통 + B 물통 <= B 물통 최대 용량
+        queue.append((x, z + y, 0))  # C 물통에 있는 물 B 물통에 다 때려넣음
 
-for i, v in enumerate(result):
-    if v > 0:
-        print(i, end=' ')
+[print(x, end=' ') for x in sorted(result)]
