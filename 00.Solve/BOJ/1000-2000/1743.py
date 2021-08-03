1b920c2d48637531477173f0c99499db59223c3f
H43RO <h2is1234@naver.com>
2021-08-04 01:09:48 +0900

    [BOJ] 5014, 1743 해설

--- 00.Solve/BOJ/1000-2000/1743.py
+++ 00.Solve/BOJ/1000-2000/1743.py
@@ -8,7 +8,7 @@ dy = [1, -1, 0, 0]
 def bfs(graph, x, y):
     queue = deque([(x, y)])
     graph[x][y] = 0
-    result = 1
+    result = 1  # 총 면적 담을 변수
     while queue:
         x, y = queue.popleft()
         for i in range(4):
@@ -17,8 +17,9 @@ def bfs(graph, x, y):
             if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] != 0:
                 queue.append((nx, ny))
                 graph[nx][ny] = 0
-                result += 1
+                result += 1  # 면적 1 증가
 
+    # 면적 반환
     return result
 
 
@@ -30,11 +31,9 @@ result = 0
 for _ in range(k):
     x, y = map(int, stdin.readline().split())
     graph[x - 1][y - 1] = 1
-    waste.append((x - 1, y - 1))
+    waste.append((x - 1, y - 1))  # 음식물 쓰레기 좌표 모두 저장
 
 for x in waste:
     i, j = x
-    if graph[i][j] == 1:
-        result = max(result, bfs(graph, i, j))
-
+    result = max(result, bfs(graph, i, j))  # 면적 저장
 print(result)
--- 00.Solve/BOJ/5000-6000/5014.py
+++ 00.Solve/BOJ/5000-6000/5014.py
@@ -1,19 +1,19 @@
 from sys import stdin
 from collections import deque
 
+# F : 층수, S : 현 위치, G : 스타트링크 위치, U : 위로 가는 층 수, D: 아래로 가는 층 수
 F, S, G, U, D = map(int, stdin.readline().split())
-count = [-1] * (F + 1)
-dx = [U, -D]
+visited = [-1] * (F + 1)
+dx = [U, -D]  # 이동 가능 벡터 : U 층만큼 올라가기, D 층만큼 내려가기
 
-# BFS
-queue = deque([S])
-count[S] = 0
+queue = deque([S])  # 시작 위치 큐잉
+visited[S] = 0  # 각 층까지의 최단거리 (최소 버튼 누른 수) 저장
 while queue:
     x = queue.popleft()
     for i in range(2):
         nx = x + dx[i]
-        if 1 <= nx <= F and count[nx] == -1:
+        if 1 <= nx <= F and visited[nx] == -1:  # 범위 안 벗어나고 방문한 적 없는 층이면
             queue.append(nx)
-            count[nx] = count[x] + 1
+            visited[nx] = visited[x] + 1
 
-print(count[G] if count[G] >= 0 else "use the stairs")
+print(visited[G] if visited[G] >= 0 else "use the stairs")
