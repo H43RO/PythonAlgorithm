7d8b4ddd87c55b4db21e256f14de56426cb993dd
H43RO <h2is1234@naver.com>
2021-08-03 21:04:12 +0900

    [BOJ] 13305, 2437, 20055 해설

--- 00.Solve/BOJ/13000-14000/13305.py
+++ 00.Solve/BOJ/13000-14000/13305.py
@@ -1,19 +1,15 @@
-from sys import stdin, stdout
+from sys import stdin
 
 n = int(stdin.readline())
 distance = list(map(int, stdin.readline().split()))
 price = list(map(int, stdin.readline().split()))
 
-min_price = price[0]
-result = 0
+min_price = price[0]  # 최소 비용 (최소 비용 만날 때마다 갱신)
+result = distance[0] * min_price  # 처음엔 무조건 기름 넣어야 함
 
-for i in range(n - 1):
-    if i == 0:
-        result += distance[0] * price[0]
-    elif min_price > price[i]:
+for i in range(1, n - 1):
+    if min_price > price[i]:  # 최소 비용 만나면 갱신
         min_price = price[i]
-        result += distance[i] * price[i]
-    else:
-        result += distance[i] * min_price
+    result += distance[i] * min_price  # 도시 이동할 때마다 최소비용으로 주유
 
 print(result)
--- 00.Solve/BOJ/2000-3000/2437.py
+++ 00.Solve/BOJ/2000-3000/2437.py
@@ -1,12 +1,12 @@
 from sys import stdin
 
 n = int(stdin.readline())
-weight = list(map(int, stdin.readline().split()))
+weight = sorted(list(map(int, stdin.readline().split())))
 
-result = 1
+result = 1  # 추의 누적합 저장
 
-for x in sorted(weight):
-    if result >= x:
-        result += x
+for x in weight:          # 무게 순으로 정렬한 추 리스트에 대하여
+    if result >= x:       # 누적합 보다 작거나 같은 무게의 추를 만났을 때
+        result += x       # 이 추의 무게를 누적합에 더해줌
 
 print(result)
