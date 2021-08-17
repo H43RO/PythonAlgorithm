e92b72aff0cccf02d6f627dd9b5e48067ab6fd8d
haero_kim <mac-pro@kakao.com>
2021-08-18 03:39:48 +0900

    [BOJ] 1051, 1449, 2210 해설

--- 00.Solve/BOJ/2000-3000/2210.py
+++ 00.Solve/BOJ/2000-3000/2210.py
@@ -8,22 +8,22 @@ dy = [1, -1, 0, 0]
 
 def dfs(graph, x, y, number):
     global result
-    if len(number) == 6:
-        if number not in result:
+    if len(number) == 6:  # 숫자 길이가 6이 되었다면
+        if number not in result:  # 중복체크 통과 시 result 에 추가
             result.append(number)
-        return
+        return  # 재귀 종료
 
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < 5 and 0 <= ny < 5:
-            dfs(graph, nx, ny, number + graph[nx][ny])
+        if 0 <= nx < 5 and 0 <= ny < 5:  # 그래프 범위 내 일때
+            dfs(graph, nx, ny, number + graph[nx][ny])  # 탐색한 숫자를 뒤에 덧붙임
 
 
-result = []
+result = []  # 가능한 모든 경우의 수를 담는 리스트
 graph = [list(map(str, stdin.readline().split())) for _ in range(5)]
 
-for i in range(5):
+for i in range(5):  # 만들 수 있는 모든 경우를 순회
     for j in range(5):
         dfs(graph, i, j, graph[i][j])
 
