bb790d334589adb16fbc90bba1e30ef06dd3df13
H43RO <h2is1234@naver.com>
2021-08-29 20:35:41 +0900

    [프로그래머스] 캐시

---
+++ 00.Solve/Programmers/Level_2/캐시.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+
+def solution(cacheSize, cities):
+    answer = 0
+    cache = deque([])
+
+    for x in cities:
+        city = x.lower()
+        if city in cache:
+            answer += 1
+            cache.remove(city)
+            cache.appendleft(city)
+        else:
+            answer += 5
+            cache.appendleft(city)
+            if len(cache) > cacheSize:
+                cache.pop()
+    return answer
+
+
+print(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+print(solution(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]))
+print(solution(2,
+               ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork",
+                "Rome"]))
+print(solution(5,
+               ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork",
+                "Rome"]))
+print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))
+print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
