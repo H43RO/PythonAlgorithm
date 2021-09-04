52c0cd7ead394bba9013904eb20342e6a7982d85
H43RO <h2is1234@naver.com>
2021-09-05 01:12:55 +0900

    [BOJ] 2559

---
+++ 00.Solve/BOJ/2000-3000/2559.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+n, k = map(int, stdin.readline().split())
+data = list(map(int, stdin.readline().split()))
+
+cum = sum(data[:k])
+result = cum
+
+for i in range(0, len(data) - k):
+    cum = cum - data[i] + data[i + k]
+    result = max(result, cum)
+
+print(result)
