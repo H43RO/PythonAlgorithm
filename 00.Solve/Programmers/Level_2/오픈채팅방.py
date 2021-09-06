9f840663bc00f1d155cf62a0423dd326959efa6b
H43RO <h2is1234@naver.com>
2021-09-06 21:05:33 +0900

    [프로그래머스] 오픈채팅방, 자물쇠와 열쇠, 합승택시 요금 해설

--- 00.Solve/Programmers/Level_2/오픈채팅방.py
+++ 00.Solve/Programmers/Level_2/오픈채팅방.py
@@ -1,12 +1,14 @@
 def solution(record):
     answer = []
     user = {}
+    # 최종 닉네임이 저장될 때 까지 갱신
     for x in record:
-        if x.startswith("Leave"):
+        if x.startswith("Leave"):  # 톡방 나가는거 노상관
             continue
         cmd, uid, nickname = x.split()
-        if cmd == "Enter" or cmd == "Change":
-            user[uid] = nickname
+        user[uid] = nickname
+
+    # 최종 닉네임을 활용하여 기록대로 출력
     for x in record:
         if x.startswith("Leave"):
             cmd, uid = x.split()
@@ -21,3 +23,7 @@ def solution(record):
 
 print(solution(
     ["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"]))
+
+# 2019 카카오 블라인드 채용 1번 문제
+# 정답률 : 59.91%
+# - 7분 소요
--- 00.Solve/Programmers/Level_3/합승_택시_요금.py
+++ 00.Solve/Programmers/Level_3/합승_택시_요금.py
@@ -6,27 +6,32 @@ INF = int(1e9)
 def solution(n, s, a, b, fares):
     graph = [[INF] * (n + 1) for _ in range(n + 1)]
 
+    # 자기 자신의 최단거리는 항상 0
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             if i == j:
                 graph[i][j] = 0
 
+    # 양방향 간선 처리
     for x in fares:
         i, j, k = x
         graph[i][j] = k
         graph[j][i] = k
 
+    # 플로이드 워셜 점화식 처리
     for k in range(1, n + 1):
         for i in range(1, n + 1):
             for j in range(1, n + 1):
                 graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
+    # 경유하는 지점을 하나씩 바꿔가며 최단거리 탐색
     result = sys.maxsize
     for i in range(1, n + 1):
         if i == a or i == b or i == s:
             continue
         result = min(result, graph[a][i] + graph[i][b] + graph[i][s])
 
+    # 엣지 케이스 처리 (e.g. 합승 안 할 때 더 빠른 경우, B 로 가는 길에 A 가 있는 경우 등)
     result = min(result, graph[s][a] + graph[s][b], graph[a][b] + graph[b][s], graph[a][b] + graph[a][s])
 
     return result
@@ -40,4 +45,4 @@ print(solution(6, 4, 5, 6, [[2, 6, 6], [6, 3, 7], [4, 6, 7], [6, 5, 11], [2, 5, 
 
 # 2021 카카오 블라인드 공채 4번
 # 정답률 : 정확도 9.60%, 효율성 7.45%
-# - 30분 소요 (정확도, 효율성 홀인원)
+# - 30분 소요
