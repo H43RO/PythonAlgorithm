08e0ee8e12e5dcaab3b72c23ea2d9dc747508efc
H43RO <h2is1234@naver.com>
2021-09-06 21:14:39 +0900

    [프로그래머스] 캐시 해설

--- 00.Solve/Programmers/Level_2/캐시.py
+++ 00.Solve/Programmers/Level_2/캐시.py
@@ -3,13 +3,13 @@ from collections import deque
 
 def solution(cacheSize, cities):
     answer = 0
-    cache = deque([], maxlen=cacheSize)
+    cache = deque([], maxlen=cacheSize)  # 캐시 (덱) 최대 크기 지정 가능
 
     for x in cities:
-        city = x.lower()  # 대소문자 구분 X
+        city = x.lower()  # 대소문자 구분 X (이 조건 놓쳐서 한 번 오답 받음)
         if city in cache:  # 캐시 히트
             answer += 1
-            cache.remove(city)
+            cache.remove(city)  # 맨 앞으로 옮겨주기 위해서 삭제
         else:  # 캐시 미스
             answer += 5
         cache.appendleft(city)
@@ -26,3 +26,7 @@ print(solution(5,
                 "Rome"]))
 print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))
 print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+
+# 2018 카카오 블라인드 공채 1번
+# 정답률 : 45.26%
+# - 9분 소요
