d90feb11a2469c7d6782e78ee8e437722305a493
haero_kim <mac-pro@kakao.com>
2021-09-23 13:03:57 +0900

    [BOJ] 14225, 15661 해설

--- 00.Solve/BOJ/14000-15000/14225.py
+++ 00.Solve/BOJ/14000-15000/14225.py
@@ -5,11 +5,11 @@ n = int(stdin.readline())
 s = list(map(int, stdin.readline().split()))
 
 sum_data = set()
-for i in range(1, n + 1):
+for i in range(1, n + 1):  # 1개 ~ N개 까지의 모든 쌍의 조합을 탐색
     for x in list(combinations(s, i)):
         sum_data.add(sum(x))
 
-for i in range(1, sum(s) + 2):
-    if i not in sum_data:
+for i in range(1, sum(s) + 2):  # 최악의 경우 모든 숫자들의 합 + 1 까지 갈 수 있음
+    if i not in sum_data:  # 1부터 모든 숫자들의 합 + 1 까지 완전탐색
         print(i)
         exit(0)
--- 00.Solve/BOJ/15000-16000/15661.py
+++ 00.Solve/BOJ/15000-16000/15661.py
@@ -7,17 +7,19 @@ member = [i for i in range(n)]
 score = [list(map(int, stdin.readline().split())) for _ in range(n)]
 
 result = sys.maxsize
+# 전체 인원의 절반까지 조합 조사 -> 반/반으로 나누는 상황까지 고려
 for i in range(1, int((n / 2) + 1)):
-    team = combinations(member, i)
+    team = combinations(member, i)  # i 명 끼리 팀 꾸리는 조합
     temp = sys.maxsize
 
-    for x in team:
-        start = list(x)
-        link = list(set(member) - set(start))
+    for x in team:  # i 명 끼리 팀 꾸리는 조합 하나하나 탐색
+        start = list(x)  # i 명 팀을 스타트 팀으로
+        link = list(set(member) - set(start))  # 나머지 인원을 모두 링크 팀으로
 
         start_ability = 0
         link_ability = 0
 
+        # 팀 내의 모든 쌍에 대해 능력치 조사 (항상 2명씩 짝지음)
         for x in combinations(start, 2):
             start_ability += score[x[0]][x[1]]
             start_ability += score[x[1]][x[0]]
@@ -25,6 +27,7 @@ for i in range(1, int((n / 2) + 1)):
             link_ability += score[x[0]][x[1]]
             link_ability += score[x[1]][x[0]]
 
+        # 스타트 팀이랑 링크 팀 능력치 차이 최소화
         temp = min(temp, abs(start_ability - link_ability))
     result = min(result, temp)
 
