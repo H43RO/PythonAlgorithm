27ddc576e67ce25e8099a1cbd880e28272abedb6
haero_kim <mac-pro@kakao.com>
2021-09-28 20:43:55 +0900

    [BOJ] 2110, 6236 해설

--- 00.Solve/BOJ/6000-7000/6236.py
+++ 00.Solve/BOJ/6000-7000/6236.py
@@ -4,8 +4,8 @@ n, m = map(int, stdin.readline().split())
 
 data = [int(stdin.readline()) for _ in range(n)]
 
-start = 0
-end = maxsize
+# 인출 금액 파라메트릭 서치
+start, end = 0, maxsize
 result = maxsize
 
 while start <= end:
@@ -23,10 +23,9 @@ while start <= end:
             count += 1  # 인출 횟수 증가
         money -= x  # 충분하면 그대로 사용
 
-    if count > m:  # 인출 횟수 초과
-        start = mid + 1  # 범위 뒤로 빼서 탐색 (mid 를 늘려보기)
-    else:
-        end = mid - 1  # 충분했다면 범위를 앞으로 빼서 탐색 (mid 를 최소화해보기)
+    if count <= m:  # M번 보다 적거나 같게 인출했다면
+        end = mid - 1  # 범위를 앞으로 빼서 탐색 (인출 금액을 최소화해보기)
         result = min(result, mid)
-
+    else:  # 인출 횟수 초과
+        start = mid + 1  # 범위 뒤로 빼서 탐색 (인출 금액을 늘려보기)
 print(result)
