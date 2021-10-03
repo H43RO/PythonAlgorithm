a380191e49b31fdc02b98dbae771334aca8a08a5
H43RO <h2is1234@naver.com>
2021-10-03 23:47:29 +0900

    [BOJ] 15988

--- 00.Solve/BOJ/15000-16000/15988.py
+++ 00.Solve/BOJ/15000-16000/15988.py
@@ -1,7 +1,18 @@
 from sys import stdin
 
-dp = [1 for _ in range(1_000_001)]
+dp = [0] * 1000001
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+dp[4] = 7
+
+mod = 1_000_000_009
+
+for i in range(5, 1000001):
+    dp[i] = (dp[i - 3] + dp[i - 2] + dp[i - 1]) % mod
 
 T = int(stdin.readline())
-for _ in ragne(T):
-    n = int(stdin.readline())
+
+for _ in range(T):
+    print(dp[int(stdin.readline())] % mod)
