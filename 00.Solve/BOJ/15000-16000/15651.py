9b5bd7b2c40b83f0658e3954195f313013a7e253
H43RO <h2is1234@naver.com>
2021-10-07 17:01:19 +0900

    [BOJ] 15651 해설

--- 00.Solve/BOJ/15000-16000/15651.py
+++ 00.Solve/BOJ/15000-16000/15651.py
@@ -1,25 +1,16 @@
-from sys import stdin, stdout
+from sys import stdin
 
 n, m = map(int, stdin.readline().split())
-
 data = [0] * (m + 1)
 
 
 def solve(data, count):
-    if count > m:
-        for i in range(1, m + 1):
-            print(data[i], end=' ')
-        print()
+    if count > m:  # 수열이 m 만큼 채워졌을 때
+        print(*data[1:])
     else:
-        for i in range(1, n + 1):
+        for i in range(1, n + 1):  # 각 자리에서 모든 경우의 수 가지 뻗기
             data[count] = i
             solve(data, count + 1)
 
 
 solve(data, 1)
-
-
-
-
-
-
