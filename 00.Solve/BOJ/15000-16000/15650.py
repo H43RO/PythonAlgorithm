fab9e1276b0bcbf24b3e7ad1d706c312ff528719
H43RO <h2is1234@naver.com>
2021-10-13 17:58:02 +0900

    [BOJ] 2792, 15650 해설

--- 00.Solve/BOJ/15000-16000/15650.py
+++ 00.Solve/BOJ/15000-16000/15650.py
@@ -6,6 +6,10 @@ data = [0] * (m + 1)
 
 
 def is_promising(data, i):
+    """
+    현재까지 수열이 비내림차순인지 검사하는 Pruning 함수
+    :return: 조건에 만족하면 True, 아니라면 False
+    """
     for j in range(1, i + 1):
         if data[j] == data[i] and i != j or data[j - 1] > data[j]:
             return False
@@ -13,21 +17,18 @@ def is_promising(data, i):
 
 
 def solve(data, count):
-    if count > m:
+    """
+    가능한 모든 수열을 만드는 함수
+    """
+    if count > m:  # 만약 M 길이의 수열이 완성되면
         for i in range(1, m + 1):
-            print(data[i], end=' ')
+            print(data[i], end=' ')  # 출력
         print()
     else:
         for i in range(1, n + 1):
-            data[count] = i
-            if is_promising(data, count):
-                solve(data, count + 1)
+            data[count] = i  # 1 ~ N 까지의 숫자 차례대로 입력
+            if is_promising(data, count):  # 만약 유망한 경우의 수가 맞다면
+                solve(data, count + 1)  # 이어나감
 
 
 solve(data, 1)
-
-
-
-
-
-
--- 00.Solve/BOJ/2000-3000/2792.py
+++ 00.Solve/BOJ/2000-3000/2792.py
@@ -9,16 +9,20 @@ end = max(jewel)
 
 result = 0
 
+# 최소 질투심에 대한 파라메트릭 서치
 while start <= end:
     mid = (start + end) // 2
 
     temp = 0
-    for x in jewel:
+    for x in jewel:  # 각 색상별 보석 개수를 인원수대로 나눠보기
         temp += math.ceil(x / mid)
 
+    # 만약 N 보다 크다면 N 명보다 더 많은 사람이 보석을 가져갔다는 건데
+    # 이는 문제 조건에 위반되기 때문에 질투심을 더 높여야 함
+    # * 즉, 한 명이 가져가는 보석 개수를 늘려야 함
     if temp > n:
         start = mid + 1
-    else:
+    else:  # N 보다 작거나 같은 사람이 보석을 가져간 경우 결과 저장
         result = mid
         end = mid - 1
 
