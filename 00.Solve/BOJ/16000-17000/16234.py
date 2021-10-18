3e848a439c323cd12444946c2bdb8a727cc71096
H43RO <h2is1234@naver.com>
2021-10-19 04:31:01 +0900

    [BOJ] 5212, 16234 해설

--- 00.Solve/BOJ/16000-17000/16234.py
+++ 00.Solve/BOJ/16000-17000/16234.py
@@ -8,32 +8,29 @@ dy = [0, 0, -1, 1]
 def bfs(visited, i, j):
     """
     시작점으로부터 열 수 있는 모든 국경선 개방
+    - 묶을 수 있는 연합 모두 저장후 마지막에 인구 이동 시행
     """
     visited[i][j] = True
     queue = deque([(i, j)])
     union_list = [(i, j)]  # 연합을 이루는 국가 (좌표) 리스트
+    people = graph[i][j]  # 총 인구수
 
     while queue:
         x, y = queue.popleft()
-
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-
             if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
-                if L <= abs(graph[x][y] - graph[nx][ny]) <= R:
+                if L <= abs(graph[x][y] - graph[nx][ny]) <= R:  # 연합 가능한 인구 차이
+                    union_list.append((nx, ny))  # 연합 리스트에 추가
+                    people += graph[nx][ny]  # 인구수 데이터 추가
                     visited[nx][ny] = True
-                    union_list.append((nx, ny))
                     queue.append((nx, ny))
 
-    if union_list:
-        people = 0
+    if union_list:  # BFS 탐색이 모두 끝난 뒤 연합 리스트 결과
+        union_people = people // len(union_list)  # 인구 이동값
         for x, y in union_list:
-            people += graph[x][y]
-
-        union_people = people // len(union_list)
-        for x, y in union_list:
-            graph[x][y] = union_people
+            graph[x][y] = union_people  # 연합 리스트 각각에 인구 이동 적용
 
     return True if len(union_list) > 1 else False  # 연합이 시작점 단 하나일 경우 연합 없는 것
 
--- 00.Solve/BOJ/5000-6000/5212.py
+++ 00.Solve/BOJ/5000-6000/5212.py
@@ -11,50 +11,52 @@ island = []
 for i in range(r):
     temp = list(stdin.readline().strip())
     for j in range(c):
-        if temp[j] == 'X':
+        if temp[j] == 'X':  # 섬의 좌표 모두 저장
             island.append((i, j))
     graph.append(temp)
 
 data = copy.deepcopy(graph)
-for x, y in island:
+for x, y in island:  # 섬 좌표 각각에서부터 인근 상하좌우 검사
     sea = 0
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
 
         if 0 <= nx < r and 0 <= ny < c:
-            if data[nx][ny] == '.':
+            if data[nx][ny] == '.':  # 인접 바다 개수 세기
                 sea += 1
-        else:
+        else:  # 지도 범위 벗어나는 곳도 바다
             sea += 1
 
-    if sea >= 3:
+    if sea >= 3:  # 만약 인접 3면 이상이 바다라면 침몰
         graph[x][y] = '.'
 
+
+# 섬을 포함하는 가장 작은 직사각형의 네 꼭짓점 찾을 것
 start_row = 0
 end_row = 0
-
 start_col = c - 1
 end_col = 0
 
 for i in range(r):
-    if 'X' in graph[i]:
+    if 'X' in graph[i]:  # 섬이 최초로 등장하는 가장 위의 행 저장
         start_row = i
         break
 
 for i in range(r - 1, -1, -1):
-    if 'X' in graph[i]:
+    if 'X' in graph[i]:  # 섬이 최초로 등장하는 가장 아래의 행 저장
         end_row = i
         break
 
 for i in range(start_row, end_row + 1):
-    tmp = [i for i, v in enumerate(graph[i]) if v == 'X']
-    if not tmp:
+    tmp = [i for i, v in enumerate(graph[i]) if v == 'X']  # 각 열에서 섬 인덱스 추출
+    if not tmp:  # 모든 열에 섬이 없다면 패스
         continue
 
-    start_col = min(start_col, tmp[0])
-    end_col = max(end_col, tmp[-1])
+    start_col = min(start_col, tmp[0])  # 섬이 최초로 등장하는 가장 왼쪽 열 저장 (갱신
+    end_col = max(end_col, tmp[-1])  # 섬이 최초로 등장하는 가장 오른쪽 열 저장 (갱신)
 
+# 완성한 네 꼭짓점을 통해 해당 직사각형만큼 출력
 for i in range(start_row, end_row + 1):
     for j in range(start_col, end_col + 1):
         print(graph[i][j], end='')
