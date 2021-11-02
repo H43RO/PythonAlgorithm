cab8f44df4a3604a98f4e801eb6f5241068d9c36
H43RO <h2is1234@naver.com>
2021-11-02 15:22:16 +0900

    [BOJ] 16926

--- 00.Solve/BOJ/16000-17000/16926.py
+++ 00.Solve/BOJ/16000-17000/16926.py
@@ -1,7 +1,17 @@
 from sys import stdin
 
+
+def move(x, y):
+    global temp, graph
+
+    previous = graph[x][y]
+    graph[x][y] = temp
+    temp = previous
+
+
 n, m, r = map(int, stdin.readline().split())
 graph = [list(map(int, stdin.readline().split())) for _ in range(n)]
+temp = graph[0][0]
 
 for _ in range(r):
     for i in range(min(n, m) // 2):
@@ -10,26 +20,18 @@ for _ in range(r):
 
         for j in range(i + 1, n - i):  # 좌
             x = j
-            previous = graph[x][y]
-            graph[x][y] = temp
-            temp = previous
+            move(x, y)
 
         for j in range(i + 1, m - i):  # 하
             y = j
-            previous = graph[x][y]
-            graph[x][y] = temp
-            temp = previous
+            move(x, y)
 
         for j in range(i + 1, n - i):  # 우
             x = n - j - 1
-            previous = graph[x][y]
-            graph[x][y] = temp
-            temp = previous
+            move(x, y)
 
         for j in range(i + 1, m - i):  # 상
             y = m - j - 1
-            previous = graph[x][y]
-            graph[x][y] = temp
-            temp = previous
+            move(x, y)
 
 [print(*graph[i]) for i in range(n)]
