2e3d5ebad149d84ba5aabefa500d81509a1f6378
H43RO <h2is1234@naver.com>
2021-11-04 17:54:48 +0900

    [BOJ] 1699

---
+++ 00.Solve/BOJ/1000-2000/1699.py
@@ -0,0 +1,12 @@
+from sys import stdin
+from math import sqrt
+
+n = int(stdin.readline())
+dp = [0] * (n + 1)  # 편의상 1-Based 인덱싱
+
+for i in range(1, n + 1):
+    dp[i] = i
+    for j in range(1, int(sqrt(i)) + 1):  # 가능한 모든 제곱수 만들어보기
+        dp[i] = min(dp[i], dp[i - j * j] + 1)  # 최솟값 갱신
+
+print(dp[n])
