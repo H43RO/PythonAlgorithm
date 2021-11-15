3e58f065260bb490235fceec54344fab47c6a953
haero_kim <mac-pro@kakao.com>
2021-11-16 02:06:07 +0900

    [BOJ] 18405

---
+++ 00.Solve/BOJ/18000-19000/18405.py
@@ -0,0 +1,44 @@
+from sys import stdin
+from collections import deque
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs():
+    count = 0
+    while queue:
+        if count == count_max:
+            break
+        for _ in range(len(queue)):
+            x, y = queue.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 0:
+                    graph[nx][ny] = graph[x][y]
+                    queue.append((nx, ny))
+        count += 1
+
+    return graph[virus_x - 1][virus_y - 1]
+
+
+n, k = map(int, stdin.readline().split())
+
+graph = []
+virus = []
+
+for i in range(n):
+    temp = list(map(int, stdin.readline().split()))
+    for j in range(n):
+        if temp[j] != 0:
+            virus.append((temp[j], i, j))
+    graph.append(temp)
+
+queue = deque([])
+for num, x, y in sorted(virus):  # 바이러스 우선순위대로 정렬 (첫 원소 기준)
+    queue.append((x, y))
+
+count_max, virus_x, virus_y = map(int, stdin.readline().split())
+
+print(bfs())
