a701df9cc1ae767987ea4a83162799eec6204ba3
haero_kim <mac-pro@kakao.com>
2021-11-18 15:43:08 +0900

    [BOJ] 18405 일부 수정

--- 00.Solve/BOJ/18000-19000/18405.py
+++ 00.Solve/BOJ/18000-19000/18405.py
@@ -8,7 +8,7 @@ dy = [0, 0, -1, 1]
 def bfs():
     count = 0
     while queue:
-        if count == count_max:
+        if count == count_max:  # 입력으로 주어진 초 도달하면 탐색 종료
             break
         for _ in range(len(queue)):
             x, y = queue.popleft()
@@ -16,11 +16,11 @@ def bfs():
                 nx = x + dx[i]
                 ny = y + dy[i]
                 if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 0:
-                    graph[nx][ny] = graph[x][y]
+                    graph[nx][ny] = graph[x][y]  # 바이러스 번호 그대로 전파
                     queue.append((nx, ny))
-        count += 1
+        count += 1  # 초 세기
 
-    return graph[virus_x - 1][virus_y - 1]
+    return graph[virus_x - 1][virus_y - 1]  # 입력으로 주어진 (x, y) 값 반환
 
 
 n, k = map(int, stdin.readline().split())
@@ -32,11 +32,11 @@ for i in range(n):
     temp = list(map(int, stdin.readline().split()))
     for j in range(n):
         if temp[j] != 0:
-            virus.append((temp[j], i, j))
+            virus.append((temp[j], i, j))  # 바이러스의 번호, 좌표 저장
     graph.append(temp)
 
 queue = deque([])
-for num, x, y in sorted(virus):  # 바이러스 우선순위대로 정렬 (첫 원소 기준)
+for num, x, y in sorted(virus):  # 바이러스 우선순위대로 정렬 (바이러스 번호 기준)
     queue.append((x, y))
 
 count_max, virus_x, virus_y = map(int, stdin.readline().split())
